d164b51aa31aeb3bc362c44b9b1874d22c9584f7
shiney5213 <shiney5213@gmail.com>
2020-05-19 17:46:33 +0900

    Update views.py

--- 4.Web/teamproject/highlight/views.py
+++ 4.Web/teamproject/highlight/views.py
@@ -3,21 +3,36 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, JsonResponse
 from django.conf import settings
 from django.views import View
+from . import apps
 
 # Create your views here.
 
-
 # def index(request):
 #     # return HttpResponse('만들기 성공')
-#     return render(request, 'highlight/play.html')
+
+# # View class 이용
+# class videoView(View):
+#     def get(self, request):
+#         return render(request, 'highlight/play.html')
+
+#     def post(self, request):
+#         data = request.GET['time']
+#         print('post', data)
+#         data = {time : data}
+#         return JsonResponse(data)
+
+       
+
+# 함수 따로 만들기
+def input(request):
+    return render(request, 'highlight/play_videotag.html')
 
 
-class videoView(View):
-    def get(self, request):
-        return render(request, 'highlight/play.html')
+def output(request):
+    starttime = request.GET.get('starttime', '0')
+    endtime = request.GET.get('endtime', '0')
 
-    def post(self, request):
-        return HttpResponse('잘 받앗음!')
-
-        # return render(request, 'highlight/play.html')
-
+    data = {'starttime' : starttime, 'endtime': endtime}
+    print(data)
+    # return render(request, 'heighlight/result.html', data)
+    return JsonResponse(data)
