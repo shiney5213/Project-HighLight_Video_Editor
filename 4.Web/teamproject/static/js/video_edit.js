4daf9d3333eb1160b018317e012b580f3e923b77
shiney5213 <shiney5213@gmail.com>
2020-05-21 17:46:01 +0900

    Update video_edit.js

--- 4.Web/teamproject/static/js/video_edit.js
+++ 4.Web/teamproject/static/js/video_edit.js
@@ -5,7 +5,9 @@
  
 //return a jQuery object
 var $video = $('#myVideo');
+var $video2 = $('#myVideo');
 
+/////1. 버튼으로 조작
 //Play/Pause control clicked
 $('.btnPlay').on('click', function() {
     if($video[0].paused) {
@@ -17,9 +19,12 @@ $('.btnPlay').on('click', function() {
     return false;
 });
 
+/////2. 비디오에서 조작
 //get HTML5 video time duration
 $video.on('loadedmetadata', function() {
     $('.duration').text($video[0].duration);
+
+
 });
  
 //update HTML5 video current play time
@@ -44,6 +49,7 @@ $video.on('timeupdate', function() {
 });
 
 
+/////3. progressBar에서 조작
 //Drag, search
 var $timeDrag = false;   /* Drag status */
 $('.progressBar').mousedown(function(e) {
@@ -81,3 +87,160 @@ var $updatebar = function(x) {
     $('.timeBar').css('width', $percentage+'%');
     $video[0].currentTime = $maxduration * $percentage / 100;
 };
+
+//////////////////////////////////////////////////////
+//start, end time select in progress bar
+var $start_time_array = [];
+var $end_time_array = [];
+var $start_time_array2=[];
+var $end_time_array2 = [];
+var $time_array=[];
+var $time_array2=[];
+var $canvas_position = [];
+
+//javascript
+// var canvas = $('.myCanvas')[0];
+// var ctx = canvas.getContext('2d');
+
+var $timeDrag2 = false;   /* Drag status */
+
+//update Progress Bar control
+var $canvas_color = '';
+var $updatebar2 = function(x) {
+    var $progress2 = $('.progressBar2');
+    var $maxduration2 = $video[0].duration; //Video duraiton
+    var $position = x - $progress2.offset().left; //Click pos
+    var $percent = 100 * $position / $progress2.width();
+    
+    $time_array.push($maxduration2 * $percent / 100);
+    $canvas_position.push($position);
+
+
+    //배열에 같은 값 제거
+    $.each($time_array,function(i,value){
+        if($time_array2.indexOf(value) == -1 ) $time_array2.push(value);
+    });
+
+    
+
+    //Check within range
+    if($percent > 100) {
+        $percent = 100;
+    }
+    if($percent < 0) {
+        $percent = 0;
+    }
+    if($status=='start'){
+        
+        $start_time_array.push($maxduration2 * $percent / 100);
+
+        //배열에 같은 값 제거
+        $.each($start_time_array,function(i,value){
+            if($start_time_array2.indexOf(value) == -1 ) $start_time_array2.push(value);
+        });
+
+        //배열 값 표시
+        $('.starttime').text($start_time_array2)
+
+    }
+    if($status=='end'){
+        $end_time_array.push($maxduration2 * $percent / 100);
+
+        //배열에 같은 값 제거
+        
+        $.each($end_time_array,function(i,value){
+            if($end_time_array2.indexOf(value) == -1 ) $end_time_array2.push(value);
+        });
+
+        //배열 값 표시
+        $('.endtime').text($end_time_array2)
+    }
+ 
+    //Update progress bar and video currenttime
+    $('.timeBar2').css('width', $percent+'%');
+    // $video[0].currentTime = $maxduration2 * $percentage2 / 100;
+};
+
+
+// var $start_time = $(.).get();
+var $status = 'start'
+$('.start_btn').on('click', function(){
+    $status = 'start';
+});
+$('.end_btn').on('click', function(){
+    $status = 'end';
+});
+
+var $timeDrag2 = false;   /* Drag status */
+if($status=='start'){
+    $('.progressBar').mousedown(function(e) {
+        $timeDrag = true;
+        $updatebar(e.pageX);
+    });
+    $('.progressBar2').mousedown(function(e) {
+        $timeDrag2 = true;
+        $updatebar2(e.pageX);
+    });
+    $(document).mouseup(function(e) {
+        if($timeDrag2) {
+            $timeDrag2 = false;
+            $updatebar2(e.pageX);   // e.pageX : 화면에서 x좌표
+        }
+    });
+    $(document).mousemove(function(e) {
+        if($timeDrag2) {
+            $updatebar2(e.pageX);
+        }
+    });
+}
+
+if($status=='end'){
+    $('.progressBar').mousedown(function(e) {
+        $timeDrag = true;
+        $updatebar(e.pageX);
+    });
+    $('.progressBar2').mousedown(function(e) {
+        $timeDrag2 = true;
+        $updatebar2(e.pageX);
+    });
+    $(document).mouseup(function(e) {
+        if($timeDrag2) {
+            $timeDrag2 = false;
+            $updatebar2(e.pageX);   // e.pageX : 화면에서 x좌표
+        }
+    });
+    $(document).mousemove(function(e) {
+        if($timeDrag2) {
+            $updatebar2(e.pageX);
+        }
+    });
+}
+
+$('.END_btn').on('click', function(){ 
+    //오름차순
+    $start_time_array2.sort(function(a, b){
+        return a - b;
+    });
+    $('.starttimearray').text($start_time_array2)
+    $end_time_array2.sort(function(a, b){
+        return a - b;
+    });
+    $('.endtimearray').text($end_time_array2)
+    $('.canvasposition').text($canvas_position)
+    $('.timearray').text($time_array)
+
+
+    $('.canvasBar').css('width' ,200 );
+    // 순서: 위, 오른쪽, 아래, 왼쪽
+    $('.canvasBar').css('margin-left' , 100);
+
+    //for문
+    for (var i = 0; i < headers.length; i++) {
+
+        alert($(headers[i]).html());
+
+    }
+
+});
+
+
